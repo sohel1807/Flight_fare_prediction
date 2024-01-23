5ab92ab4bfdb96f2c919565af793114e8bf4bd2b
sohel1807 <114603804+sohel1807@users.noreply.github.com>
2024-01-23 11:28:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ airl=['IndiGo', 'Air India', 'Jet Airways', 'SpiceJet',
 source=['Banglore', 'Kolkata', 'Delhi', 'Chennai', 'Mumbai']
 destination=['New Delhi', 'Banglore', 'Cochin', 'Kolkata', 'Delhi', 'Hyderabad']
 stops=[0,1,2,3,4]
+ months = {"January": 1,"February": 2,"March": 3, "April": 4, "May": 5,"June": 6,"July": 7,"August": 8, "September": 9,"October": 10,"November": 11,"December": 12}
 total_time=0
 col1,col2,col3,col4 =st.columns(4)
 with col1:
@@ -27,7 +28,7 @@ col5,col6 =st.columns(2)
 with col5:
     date=st.selectbox("Select Journey date:",np.arange(1,32))
 with col6:
-    month=st.selectbox("Select journey month:",np.arange(1,13))
+    month=st.selectbox("Select journey month:",list(months.keys()))
 col7,col8,col9,col10= st.columns(4)
 with col7:
     dept_time1 = st.slider("Select Departure hour", 0, 23, 0)
@@ -44,7 +45,7 @@ else:
     total_time = (24 * 60 - (dept_time1 * 60 + dept_time2)) + (arrival_time1 * 60 + arrival_time2)
 if st.button("Predict Fare"):
     input_df=pd.DataFrame({"Airline":[Airline],"Source":[source],"Destination":[Dest],
-                           "Total_Stops":[stops],"Dminutes":[abs(total_time)],"Journey_date":[date],"Journey_month":[month],
+                           "Total_Stops":[stops],"Dminutes":[abs(total_time)],"Journey_date":[date],"Journey_month":[months[month]],
                            "deph":[dept_time1],"depm":[dept_time2],"Arrivalh":[arrival_time1],"Arrivalm":[arrival_time2]})
     result=data.predict(input_df)[0]
     st.title(f"Predicted Fare :{round(result)} RS")
